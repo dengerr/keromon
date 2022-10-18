600ba6b093e28908ba296ddafedb6bad90e8f880
Denis Buryi <denis@buryi.de>
2022-10-18 09:35:00 +0700

    split habr rss to several posts

--- habr_rss.py
+++ habr_rss.py
@@ -8,6 +8,18 @@ BODY_TEMPLATE = """\
 - [ ] {voting} <a href="{url}">{text}</a>\
 """
 
+ALL_IN_ONE_POST = False
+ARTICLES_COUNT_IN_ONE_POST = 5
+
+
+def texts_generator(texts):
+    i = 0
+    while i < len(texts):
+        i1 = i * ARTICLES_COUNT_IN_ONE_POST
+        i2 = (i + 1) * ARTICLES_COUNT_IN_ONE_POST
+        yield texts[i1:i2]
+        i += ARTICLES_COUNT_IN_ONE_POST
+
 
 if __name__ == "__main__":
     current = datetime.datetime.now()
@@ -31,13 +43,26 @@ if __name__ == "__main__":
         for url in urls
         for article in get_habr_articles(url)
     ]
-    body = "<br/>\n".join(texts)
 
-    item = dict(
-        title=f"Weekly HABR {current_date}",
-        link=urls[0],
-        description=body,
-        pubDate=current,
-        guid=current_date,
-    )
-    rss.print_rss2(channel, [item])
+    items = []
+    if ALL_IN_ONE_POST:
+        body = "<br/>\n".join(texts)
+        items.append(dict(
+            title=f"Weekly HABR {current_date}",
+            link=urls[0],
+            description=body,
+            pubDate=current,
+            guid=current_date,
+        ))
+    else:
+        for mini_texts in texts_generator(texts):
+            body = "<br/>\n".join(mini_texts)
+            items.append(dict(
+                title=f"Weekly HABR {current_date}",
+                link=urls[0],
+                description=body,
+                pubDate=current,
+                guid=current_date,
+            ))
+
+    rss.print_rss2(channel, items)
